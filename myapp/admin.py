b351f829f6400451e426a4a22618560c31d69979
Dimon <ha020285mdv@gmail.com>
2022-06-10 08:32:47 +0300

    done without #3Comments

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from myapp.models import Writer, Publication, Member, Author, Book
+from myapp.models import Like, Comment, Article, Profile
 
-# Register your models here.
+
+admin.site.register(Writer)
+admin.site.register(Publication)
+
+admin.site.register(Member)
+admin.site.register(Author)
+admin.site.register(Book)
+
+admin.site.register(Profile)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Like)
+
+
+
+
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,148 @@
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models import SET_NULL, CASCADE
 
-# Create your models here.
+
+# 1. Разрабатываем каталог книг, у каждой книги обязательно есть автор, и он может быть только один.
+     #(!!!чтобы не копипастить код во 2-е задание я сделал упрощенное представление, а во втором уже старался прокачать)
+class Writer(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    dob = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '1_Author'
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey(Writer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = '1_Book'
+
+# 2. Разработать книжную библиотеку. Храним книги, храним авторов, книгу могут написать несколько соавторов.
+#    Xраним кто брал книги, и доступна ли книга сейчас.
+
+class Member(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=13, unique=True)
+    email = models.EmailField(max_length=100, blank=True)
+    date_of_joining = models.DateField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    blocked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '2_Member'
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    dob = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '2_Author'
+
+class Book(models.Model):
+    ENGLISH = 'EN'
+    UKRAINIAN = 'UA'
+    RUSSIAN = 'RU'
+    CHINESE = 'CH'
+    SPANISH = 'SP'
+    OTHER = 'OT'
+    LANGUAGE_CHOICES = [
+        (ENGLISH, 'English'),
+        (UKRAINIAN, 'Ukrainian'),
+        (RUSSIAN, 'Russian'),
+        (CHINESE, 'Chinese'),
+        (SPANISH, 'Spanish'),
+        (OTHER, 'Other'),
+    ]
+    title = models.CharField(max_length=250)
+    author = models.ManyToManyField(Author)
+    #  хотел сделать Автора обязательным, но прочитал что в случае M2M такая валидация делается на уровне контроллера
+    original_lang = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default=OTHER)
+    year_of_public = models.DateField()
+    date_of_getting = models.DateField(auto_now=True)
+    is_free = models.BooleanField(default=True)
+    date_of_status_changing = models.DateField(auto_now_add=True)
+    is_taken_by = models.ForeignKey(Member, on_delete=SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = '2_Book'
+
+# Разработать набор моделей, для сайта-блога, на котором можно выставлять свои статьи,
+#  комментировать чужие, ставить лайк и дизлайк статье, и комментарию.
+#  3.1* Доделать так, что бы связи позволяли комментировать комментарии.
+#  3.2* Сделать лайки через GenericForeignKey
+
+class Profile(models.Model):
+    login = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100, unique=True)
+    dob = models.DateField()
+    date_of_registration = models.DateField(auto_now=True)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '3_User'
+
+class Article(models.Model):
+    author = models.ForeignKey(Profile, on_delete=CASCADE)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    date_of_writing = models.DateField(auto_now=True)
+    is_verified = models.BooleanField(default=False)
+    date_of_moderation = models.DateField(auto_now_add=True)
+    is_public = models.BooleanField(default=False)
+    likes = GenericRelation('Like')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = '3_Article'
+
+class Comment(models.Model):
+    comment = models.TextField(max_length=300)
+    author = models.ForeignKey(Profile, on_delete=CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    likes = GenericRelation('Like')
+
+    def __str__(self):
+        return f"{self.author}'s comment: {self.comment[:15]}"
+
+    class Meta:
+        verbose_name = '3_Comment'
+
+class Like(models.Model):
+    author = models.ForeignKey(Profile, on_delete=CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    limit = models.Q(app_label='myapp', model='article') | models.Q(app_label='myapp', model='comment')
+    content_type = models.ForeignKey(ContentType, limit_choices_to=limit, on_delete=CASCADE)
+    object_id = models.PositiveIntegerField()
+    liked_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return f"{self.author}'s like for {self.liked_object}"
+
+    class Meta:
+        verbose_name = '3_Like'
